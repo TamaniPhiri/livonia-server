e23fa968fd353b8ba669b180993b850bba4cc809
DC-21 <cholahkuboko@gmail.com>
2023-09-26 14:40:00 +0200

    first commit

---
+++ prisma/migrations/20230926123946_product/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Transactions` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Transactions" DROP COLUMN "name",
+ADD COLUMN     "product" TEXT;
