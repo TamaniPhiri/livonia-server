aa0a90d22b3dd35259e65847b717b479b087aa1e
DC-21 <cholahkuboko@gmail.com>
2023-09-29 19:58:17 +0200

    batchId

--- src/repository/transaction-repo.js
+++ src/repository/transaction-repo.js
@@ -1,9 +1,33 @@
 const { PrismaClient } = require("@prisma/client");
 const Prisma = new PrismaClient();
 
+function generateRandomString(length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+}
+
+let batchCounter = 1; // Initialize the batch counter
+
+// Function to generate a unique batchId for each batch
+function generateBatchId() {
+  const autoIncrementPart = batchCounter.toString().padStart(4, "0"); // Auto-incrementing part
+  const randomPart = generateRandomString(4); // Random part
+
+  const batchId = `${autoIncrementPart}-${randomPart}`;
+  return batchId;
+}
+
 const TransactionRepository = () => {
   const createTransaction = async (transactionsData) => {
     const createdTransactions = [];
+    const batchId = generateBatchId();
 
     for (const data of transactionsData) {
       const createdTransaction = await Prisma.transactions.create({
@@ -13,6 +37,7 @@ const TransactionRepository = () => {
           brand: data.brand,
           quantity: parseInt(data.quantity),
           amount: data.amount,
+          batchId,
         },
       });
 
